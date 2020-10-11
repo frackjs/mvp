c1629a396964abeab5cb72b2b4ea50b3af2cd614
Andy <andy@Andys-MacBook-Pro.local>
2020-10-11 19:51:20 -0400

    camel -> kebab

--- generateRoute.js
+++ generateRoute.js
@@ -1,7 +1,30 @@
 const __ = "  ";
 
+let cache = {}
+
+const kebab = (camel) => {
+  if(camel.toLowerCase()===camel){
+    return camel
+  }
+  if(cache[camel] !== undefined){
+    return cache[camel]
+  }
+  const letters = camel.split('')
+  let out = []
+  letters.forEach(x => {
+    if(x.toLowerCase() === x) { // lower
+      out.push(x)
+    }
+    else { // upper
+      out.push('-')
+      out.push(x.toLowerCase())
+    }
+  })
+  return out.join('')
+}
+
 const generateRoute = (objName, method) => (
-  __+"app.get('/"+objName+"/"+method.name+"/:id', (req, res) => {"+'\n'+
+  __+"app.get('/"+kebab(objName)+"/"+kebab(method.name)+"/:id', (req, res) => {"+'\n'+
   __+__+method.args.map(arg => "const "+arg+" = req.params['"+arg+"']").join("\n"+__+__)+"\n"+
   __+__+"const data = "+objName+"."+method.name+"("+'\n'+
   __+__+__+method.args.map(x => x + ",").join("\n"+__+__+__)+"\n"+
