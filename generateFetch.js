97469fffa032446586e7c04bc84ca9611be1bfb6
Andy <andy@Andys-MacBook-Pro.local>
2020-10-14 11:44:54 -0400

    lint

--- generateFetch.js
+++ generateFetch.js
@@ -1,49 +1,50 @@
-const { METHODS } = require("http");
+const xx = '  ';
+const xxxx = xx + xx;
 
-const __ = "  ";
-const ____=__+__;
-const ______=__+__+__;
-
-let cache = {}
+const cache = {};
 
 const kebab = (camel) => {
-  if(camel.toLowerCase()===camel){
-    return camel
+  if (camel.toLowerCase() === camel) {
+    return camel;
   }
-  if(cache[camel] !== undefined){
-    return cache[camel]
+
+  if (cache[camel] !== undefined) {
+    return cache[camel];
   }
-  const letters = camel.split('')
-  let out = []
-  letters.forEach(x => {
-    if(x.toLowerCase() === x) { // lower
-      out.push(x)
+
+  const letters = camel.split('');
+  const out = [];
+  letters.forEach((x) => {
+    if (x.toLowerCase() === x) { // lower
+      out.push(x);
+    } else { // upper
+      out.push('-');
+      out.push(x.toLowerCase());
     }
-    else { // upper
-      out.push('-')
-      out.push(x.toLowerCase())
-    }
-  })
-  return out.join('')
+  });
+  return out.join('');
+};
+
+const httpType = 'get';
+
+function generateFetch(objName, method) {
+  const pathVars = method.args.map((arg) => (
+    arg !== 'params' ? ` + '/' + ${arg}` : ''
+  )).join('');
+
+  let out = '';
+
+  out += `${xx}${kebab(objName)}.${method.name} = function(${method.args.join(', ')}) {\n`;
+  out += `${xxxx}const path = '/${kebab(objName)}/${kebab(method.name)}'${pathVars};\n`;
+  out += `${xxxx}return axios.${httpType}(path`;
+  out += method.args[method.args.length - 1] === 'params' ? ', params' : '';
+  out += ');\n';
+  out += `${xx}};\n`;
+
+  return out;
 }
 
-const httpType = 'get'
+const out = generateFetch('products', { name: 'update', args: ['id', 'params'] });
+// eslint-disable-next-line no-console
+console.log(out);
 
-const generateFetch = (objName, method) => {
-
-  const pathVars = method.args.map(arg => {
-    return arg!=='params' ? " + '/' + " + arg : ''
-  }).join('')
-
-  return (
-__+kebab(objName)+'.'+method.name+' = function('+method.args.join(', ')+'){' +'\n'+
-____+"const path = '/" + kebab(objName) + "/" + kebab(method.name) + "'" + pathVars + ";" +'\n'+
-____+"return axios."+httpType+"(path" +
-        ((method.args[method.args.length-1]==='params') ? ', params' : '') +
-      ');' +'\n'+
-__+'};' +'\n'
-  )
-}
-
-const out = generateFetch('products', {name: 'update', args: ['id','params']})
-console.log(out)
