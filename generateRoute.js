5d0d8a2c46e3580aa7be74914b4a6b2873a0a4c6
Andy <andy@Andys-MacBook-Pro.local>
2020-10-31 13:05:44 -0400

    HTTP verbs

--- generateRoute.js
+++ generateRoute.js
@@ -24,14 +24,24 @@ const kebab = (camel) => {
 
 const renderEachArg = (arg) => `const ${arg} = req.params['${arg}']`
 
-const generateRoute = (objName, method) => (
-  `  app.get('/${kebab(objName)}/${kebab(method.name)}${method.args.filter((x) => x !== 'params').map((x) => `/:${x}`).join()}', (req, res) => {\n${
+const generateRoute = (objName, method) => {
+  let verb = 'get'
+  if (method.name.indexOf('create') === 0) {
+    verb = 'post'
+  }
+  if (method.name.indexOf('update') === 0) {
+    verb = 'put'
+  }
+  if (method.name.indexOf('delete') === 0) {
+    verb = 'delete'
+  }
+  return `  app.${verb}('/${kebab(objName)}/${kebab(method.name)}${method.args.filter((x) => x !== 'params').map((x) => `/:${x}`).join()}', (req, res) => {\n${
     xx}  ${method.args.map(renderEachArg).join(`\n    `)}\n${
     xx}  const data = ${objName}.${method.name}(\n${
     xx}    ${method.args.join(`,\n      `)}\n${
     xx}  )\n${
     xx}  res.json(data)\n${
     xx}});`
-)
+}
 
 module.exports = generateRoute
