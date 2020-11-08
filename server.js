ed74acc056b4d5f8ccf03c52329f09433e839230
Andy <andy@Andys-MacBook-Pro.local>
2020-11-08 14:31:27 -0500

    the generate folder

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const productsRoutes = require('./routes/productsRoutes')
+const productsRoutes = require('./actionsRoutes/productsRoutes')
 
 const app = express()
 const port = 3001
