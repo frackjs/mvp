04d62060b6b76c5011227056d0f07ea2ab7fc8a0
Andy <andy@Andys-MacBook-Pro.local>
2020-11-08 22:47:34 -0500

    clean up server.js

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const productsRoutes = require('./actionsRoutes/productsRoutes')
 
 const app = express()
 const port = 3001
 
 app.use(cors())
 app.use(bodyParser.json())
-
-productsRoutes(app)
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
